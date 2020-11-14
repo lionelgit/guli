21851b713d95ba9249423141ae566a1ae35c5bd6
liangyuhu <tiger941@icloud.com>
2020-11-14 23:28:42 +0800

    nacos相关

--- gulimall-coupon/src/test/java/com/atguigu/gulimall/coupon/GulimallCouponApplicationTests.java
+++ gulimall-coupon/src/test/java/com/atguigu/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -1,13 +1,21 @@
 package com.atguigu.gulimall.coupon;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootTest
+@RefreshScope
 class GulimallCouponApplicationTests {
+    @Value("${coupon.username}")
+    String username;
 
+    @Value("${coupon.age}")
+    String age;
     @Test
     void contextLoads() {
+        System.out.println(username+":"+age);
     }
 
 }
--- gulimall-member/src/test/java/com/atguigu/gulimall/member/GulimallMemberApplicationTests.java
+++ gulimall-member/src/test/java/com/atguigu/gulimall/member/GulimallMemberApplicationTests.java
@@ -1,10 +1,21 @@
 package com.atguigu.gulimall.member;
 
+import com.atguigu.common.utils.R;
+import com.atguigu.gulimall.member.feign.CouponFeignService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GulimallMemberApplicationTests {
+    @Autowired
+    CouponFeignService couponFeignService;
+
+    @Test
+    void testFeign(){
+        R membercoupons = couponFeignService.membercoupons();
+        System.out.println(membercoupons);
+    }
 
     @Test
     void contextLoads() {
